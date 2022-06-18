b4592c6ecda2f533d9633c2bd11f79267d36ed30
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2022-06-18 18:56:13 +0530

    Basic design completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,23 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-br from-emerald-200 via-rose-500 to-amber-500 flex flex-col justify-center sm:py-12">
+      <div className="min-h-screen bg-gradient-to-b from-red-600 via-red-600 to-black flex flex-col justify-center sm:py-12">
         <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-          <div className="bg-white shadow rounded-lg divide-y divide-gray-200 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 ">
-            <h1 className="font-bold text-center text-4xl mb-5 p-3">
-              <span className="text-red-600">YouTube</span> Downloader
+          <div className="bg-white font-thin shadow rounded-sm divide-y divide-gray-200 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-80 ">
+            <h1 className="font-sans text-center text-5xl mb-5 p-3 ">
+              <span className="text-red-600">YouTube</span>{" "}
+              <span>Downloader</span>
             </h1>
           </div>
-          <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+          <div className="bg-white shadow w-full rounded-sm divide-y divide-gray-200 ">
             <div className="px-5 py-7">
               <input
                 type="text"
                 className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                placeholder="Enter YouTube Video URL"
               />
               <button
                 type="button"
@@ -61,48 +62,18 @@ const Home: NextPage = () => {
                 </button>
               </div>
             </div>
-            <div className="py-5">
+            {/* <div className="py-5">
               <div className="grid grid-cols-2 gap-1">
                 <div className="text-center sm:text-left whitespace-nowrap">
-                  <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      className="w-4 h-4 inline-block align-text-top"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"
-                      />
-                    </svg>
-                    <span className="inline-block ml-1">Forgot Password</span>
+                  <button className="mx-5 px-5 py-4 font-normal text-sm rounded-lg text-gray-500 cursor-default">
+                    <span>Made by Pratham 🤓</span>
                   </button>
                 </div>
                 <div className="text-center sm:text-right whitespace-nowrap">
-                  <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      className="w-4 h-4 inline-block align-text-bottom	"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
-                      />
-                    </svg>
-                    <span className="inline-block ml-1">Help</span>
-                  </button>
+                  
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
